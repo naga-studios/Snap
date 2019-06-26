9de8ba5a7fa2b16f0828e066476d91121b0f6be9
D <dheerajk.har@gmail.com>
2019-06-25 20:34:44 -0700

    Add files via upload

---
+++ BuildingBlockScript.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingBlockScript : MonoBehaviour
+{
+    [SerializeField] private Collider[] snapPoints;        //CHILDREN SNAP POINTS ATTACHED TO THIS BLOCK 
+    [SerializeField] private List<string> snappablePoints;     //SNAPPABLE POINTS THIS BLOCK CAN SNAP TO
+    [SerializeField] private int numberOfSnapPointsToStick = 1;
+    [Space]
+    [SerializeField] Material defaultMaterial;
+    [SerializeField] Material canplaceMaterial;
+    [SerializeField] Material cannotPlaceMaterial;
+    [Space]
+    [SerializeField] private bool isAlreadyPlaced = false;
+
+    private MeshRenderer meshRenderer;
+    private Transform pointSnappedTo;
+
+    private int currentNumberOfSnapPoints = 0;
+    private bool canBePlaced = false;
+
+    private void Start()
+    {
+        meshRenderer = this.GetComponent<MeshRenderer>();
+        UpdateMesh();
+    }
+
+    public List<string> GetSnappablePoints()
+    {
+        return snappablePoints;
+    }
+
+    public void TurnOnSnapPoints()
+    {
+        for (int i = 0; i < snapPoints.Length; ++i)
+        {
+            snapPoints[i].transform.gameObject.SetActive(true);
+        }
+    }
+
+    public void TurnOffSnapPoints()
+    {
+        for (int i = 0; i < snapPoints.Length; ++i)
+        {
+            snapPoints[i].transform.gameObject.SetActive(false);
+        }
+    }
+
+    public bool Place()
+    {
+        if (canBePlaced)
+        {
+            isAlreadyPlaced = true;
+            pointSnappedTo.GetComponent<SnapPointScript>().AttachBlockHere();
+            meshRenderer.material = defaultMaterial;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+
+    public bool Snap(Transform snapLocation)
+    {
+        currentNumberOfSnapPoints++;
+        if (!isAlreadyPlaced && currentNumberOfSnapPoints >= numberOfSnapPointsToStick) {
+            this.transform.position = snapLocation.position;
+            this.transform.rotation = snapLocation.rotation;
+            Debug.Log("snapppoints: " + currentNumberOfSnapPoints);
+            pointSnappedTo = snapLocation;
+            canBePlaced = true;
+            UpdateMesh();
+            return true;
+        }
+        return false;
+    }
+
+    public void UnSnap(Transform snapLocation = null)
+    {
+        currentNumberOfSnapPoints = 0;
+        canBePlaced = false;
+        snapLocation = null;
+        UpdateMesh();
+    }
+
+    private void UpdateMesh()
+    {
+        if (canBePlaced)
+        {
+            meshRenderer.material = canplaceMaterial;
+        }
+        else
+        {
+            meshRenderer.material = cannotPlaceMaterial;
+        }
+    }
+
+}
---
+++ SnapPointScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapPointScript : MonoBehaviour
+{
+    [SerializeField] private string snapPointTag;   //SNAP POINT TAG
+    private bool containsBlock = false;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        BuildingBlockScript buildingBlockScript = collider.transform.GetComponent<BuildingBlockScript>();
+        if (!containsBlock && buildingBlockScript)
+        {
+            foreach(string tag in buildingBlockScript.GetSnappablePoints())
+            {
+                if(tag == snapPointTag)
+                {
+                    if (buildingBlockScript.Snap(this.transform))
+                    {
+                        BuildingSystemScript.instance.PutInPauseMode();
+                    }
+                }
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        BuildingBlockScript buildingBlockScript = collider.transform.GetComponent<BuildingBlockScript>();
+        if (!containsBlock && buildingBlockScript)
+        {
+            foreach (string tag in buildingBlockScript.GetSnappablePoints())
+            {
+                if (tag == snapPointTag)
+                {
+                    buildingBlockScript.UnSnap(this.transform);
+                }
+            }
+        }
+    }
+
+    //WHEN A BLOCK IS PLACED IN THE SNAPPOINT
+    public void AttachBlockHere()
+    {
+        containsBlock = true;
+    }
+
+    //WHEN A BLOCK IS REMOVED FROM THE SNAP POINT
+    public void UnattachBlockHere()
+    {
+        containsBlock = false;
+    }
+
+}
